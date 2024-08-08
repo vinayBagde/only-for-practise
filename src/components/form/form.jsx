46035cb6879ce4e775518af8cd2b1bb5cb1f1993
vinayBagde <vinaybagde0@gmail.com>
2024-08-08 23:18:24 +0530

    commit

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,1 +1,18 @@
-export default 
+import "./Form.css";
+export default function Form() {
+  let submitted = (event) => {
+    event.preventDefault();
+    console.log("form was submitted");
+  };
+  return (
+    <>
+      <div id="form">
+        <form action="./action">
+          <input type="text" placeholder="user name" />
+          <br />
+          <button onClick={submitted}>click</button>
+        </form>
+      </div>
+    </>
+  );
+}
