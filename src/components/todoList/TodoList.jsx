f7e64fd841deeffb454c9356406cba2d2d103c24
vinayBagde <vinaybagde0@gmail.com>
2024-08-25 00:12:10 +0530

    create add task button

--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -1,17 +1,30 @@
 import "./TodoList.css";
-import {useState} from "react";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 export default function TodoList() {
-    let [todos, setTodos] = useState([{task: "sample task", key: 1}])
-    let [newTodo, SetNewTodo] = useState("");
+  let [todos, setTodos] = useState([{ task: "sample task", id: uuidv4() }]);
+  let [newTodo, setNewTodo] = useState("");
 
-    let addTask = (event) => {
-        SetNewTodo(event.target.value);
-    }
-    return(
-        <>
-            <input type="text" placeholder="enter task" onChange={addTask}/>
-            <br />
-            <button>Add Task</button>
-        </>
-    );
+
+  let updateTodoValue = (event) => {
+    setNewTodo(event.target.value)
+  }
+
+  let addNewTask = () => {
+    setTodos((previousTodo) => {
+        return [...previousTodo, {task: newTodo, id: uuidv4()}]
+    })
+
+    setNewTodo("");
+  }
+  return (
+    <>
+      <input type="text" placeholder="enter task" onChange={updateTodoValue}/>
+      <br />
+      <button onClick={addNewTask} >Add task</button>
+      <br /> <br />
+      <hr />
+      {todos.map((todo) => <li key={todo.id}>{todo.task}</li>)}
+    </>
+  );
 }
