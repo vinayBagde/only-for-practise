d73f522b7aa485a64df3aea42623876bbe0508f4
vinayBagde <vinaybagde0@gmail.com>
2024-08-25 00:31:01 +0530

    add delete button

--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -5,26 +5,42 @@ export default function TodoList() {
   let [todos, setTodos] = useState([{ task: "sample task", id: uuidv4() }]);
   let [newTodo, setNewTodo] = useState("");
 
-
   let updateTodoValue = (event) => {
-    setNewTodo(event.target.value)
-  }
+    setNewTodo(event.target.value);
+  };
 
   let addNewTask = () => {
     setTodos((previousTodo) => {
-        return [...previousTodo, {task: newTodo, id: uuidv4()}]
-    })
+      return [...previousTodo, { task: newTodo, id: uuidv4() }];
+    });
 
     setNewTodo("");
+  };
+
+
+  let deleteTodo = (id) => {
+    setTodos((previousTodo) => {
+        return previousTodo.filter((todo) => todo.id != id);
+    });
   }
   return (
     <>
-      <input type="text" placeholder="enter task" onChange={updateTodoValue}/>
+      <input
+        type="text"
+        placeholder="enter task"
+        value={newTodo}
+        onChange={updateTodoValue}
+      />
       <br />
-      <button onClick={addNewTask} >Add task</button>
+      <button onClick={addNewTask}>Add task</button>
       <br /> <br />
       <hr />
-      {todos.map((todo) => <li key={todo.id}>{todo.task}</li>)}
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          <span style={{marginRight: "10px"}}>{todo.task}</span>
+          <button onClick={() => deleteTodo(todo.id)}>delete</button>
+        </li>
+      ))}
     </>
   );
 }
