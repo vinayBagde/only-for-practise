d9fefd13d142c113d31bdbd7cc501f5cf00838a2
vinayBagde <vinaybagde0@gmail.com>
2024-08-25 02:00:42 +0530

    add upper case one button

--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -25,11 +25,23 @@ export default function TodoList() {
 
   let updateAllTask = () => {
     setTodos((previousTodo) => {
-        return previousTodo.map((todo) => {
-            return {...todo, task: todo.task.toUpperCase()}
-        })
-    })
+      return previousTodo.map((todo) => {
+        return { ...todo, task: todo.task.toUpperCase() };
+      });
+    });
   };
+
+  let updateOneTask = (id) => {
+    setTodos((previousTodo) => {
+        return previousTodo.map((todo) => {
+            if(todo.id === id) {
+                return {...todo, task: todo.task.toUpperCase()}
+            } else {
+                return todo;
+            }
+        })
+    })    
+  }
   return (
     <>
       <input
@@ -38,22 +50,23 @@ export default function TodoList() {
         value={newTodo}
         onChange={updateTodoValue}
       />
-      <br />
+      <br /> <br />
       <button onClick={addNewTask}>Add task</button>
       <br /> <br />
       <hr />
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>
+          <li key={todo.id} style={{marginBottom: "10px"}}>
             <span style={{ marginRight: "10px" }}>{todo.task}</span>
             <button onClick={() => deleteTodo(todo.id)}>delete</button>
+            <button style={{ marginLeft: "10px" }} onClick={() => updateOneTask(todo.id)}>UpperCase One</button>
           </li>
         ))}
       </ul>
       <br />
       <br />
       <hr />
-      <button onClick={updateAllTask}>Update All Task</button>
+      <button onClick={updateAllTask}>UpperCase All</button>
     </>
   );
 }
